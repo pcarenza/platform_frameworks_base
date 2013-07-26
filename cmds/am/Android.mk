8d458e865acfe9e4915d623d63a6bb3a2bd95aa4
pcarenza <pcarenza@gmail.com>
2013-07-26 11:16:50 -0400

    change for android 4.3

--- cmds/am/Android.mk
+++ cmds/am/Android.mk
@@ -8,24 +8,9 @@ LOCAL_MODULE := am
 include $(BUILD_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
-ALL_PREBUILT += $(TARGET_OUT)/bin/am
-$(TARGET_OUT)/bin/am : $(LOCAL_PATH)/am | $(ACP)
-	$(transform-prebuilt-to-target)
+LOCAL_MODULE := am
+LOCAL_SRC_FILES := am
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
 
-NOTICE_FILE := NOTICE
-files_noticed := bin/am
-
-# Generate rules for a single file. The argument is the file path relative to
-# the installation root
-define make-notice-file
-
-$(TARGET_OUT_NOTICE_FILES)/src/$(1).txt: $(LOCAL_PATH)/$(NOTICE_FILE)
-	@echo Notice file: $$< -- $$@
-	@mkdir -p $$(dir $$@)
-	@cat $$< >> $$@
-
-$(TARGET_OUT_NOTICE_FILES)/hash-timestamp: $(TARGET_OUT_NOTICE_FILES)/src/$(1).txt
-
-endef
-
-$(foreach file,$(files_noticed),$(eval $(call make-notice-file,$(file))))
